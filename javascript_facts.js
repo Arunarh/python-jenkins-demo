8e635cf5a75d664e9f97cc5607669592128bcc71
arunarh <ogaarunarh@gmail.com>
2026-07-27 23:16:13 +0100

    Add Hello World JavaScript alert

--- javascript_facts.js
+++ javascript_facts.js
@@ -1,6 +1,1 @@
-console.log("FUN FACTS ABOUT JAVASCRIPT");
-console.log("1. Javascript was created by Brendan Eich in 1995.");
-console.log("2. Javascript was originally called Mocha.");
-console.log("3. Javascript and Java are two different programming languages.");
-console.log("4. Javascript can be used for both front-end and back-end development.");
-console.log("5. Javascript is widely used to make websites interactive.");
+alert("Hello, world!")
