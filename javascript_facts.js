4ef322898cb245e9d6b3ef1d68f8290e0e8edf05
arunarh <ogaarunarh@gmail.com>
2026-08-05 07:06:52 +0100

    Add rectangle area JavaScript function

--- javascript_facts.js
+++ javascript_facts.js
@@ -1,5 +1,7 @@
-for (let number = 1; number <= 20; number++) {
-    if (number % 2 !== 0) {
-        console.log(number);
-    }
+function calculateAreaOfRectangle(length, width) {
+    let area = length * width;
+    console.log(area);
 }
+
+// Test the function
+calculateAreaOfRectangle(10, 5);
