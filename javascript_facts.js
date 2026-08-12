896010b30ec5ebdb79827fe8dd0c345583388883
arunarh <ogaarunarh@gmail.com>
2026-08-12 01:37:51 +0100

    Add personal library 3 task

--- javascript_facts.js
+++ javascript_facts.js
@@ -1,14 +1,29 @@
-let books = {
-    title: "Things Fall Apart",
-    description: "A story about tradition, change, and the impact of colonialism.",
-    numberOfPages: 209,
-    author: "Chinua Achebe",
-    reading: true,
-
-    toggleReadingStatus: function() {
-        this.reading = !this.reading;
-        console.log(this.reading);
+let books = [
+    {
+        title: "Things Fall Apart",
+        description: "A story about tradition and change.",
+        numberOfPages: 209,
+        author: "Chinua Achebe",
+        reading: true
+    },
+    {
+        title: "The Alchemist",
+        description: "A story about following your dreams.",
+        numberOfPages: 208,
+        author: "Paulo Coelho",
+        reading: false
+    },
+    {
+        title: "Half of a Yellow Sun",
+        description: "A story set during the Nigerian Civil War.",
+        numberOfPages: 433,
+        author: "Chimamanda Ngozi Adichie",
+        reading: true
     }
-};
+];
 
-books.toggleReadingStatus();
+for (let i = 0; i < books.length; i++) {
+    if (books[i].reading === true) {
+        console.log(books[i]);
+    }
+}
